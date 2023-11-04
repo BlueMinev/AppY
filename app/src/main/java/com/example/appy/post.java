af74dad7beefa14b6bd93ca7d06ba73df45a60d7
Blue <74419769+BlueMinev@users.noreply.github.com>
2023-11-04 19:51:20 +0000

    Initial commit

---
+++ app/src/main/java/com/example/appy/post.java
@@ -0,0 +1,23 @@
+public class post {
+    String title = "";
+    String contents = "";
+    Integer postID;
+    Integer likes;
+    String username = "";
+
+    public void setUsername(String currentUser){
+        username = currentUser;
+    }
+
+    public void setTitle(String titleIn){
+        title = titleIn;
+    }
+
+    public void setContents(String contentsIn){
+        contents = contentsIn;
+    }
+
+    public void updateLikes(Integer changeInLikes){
+        likes = likes + changeInLikes;
+    }
+}
---
+++ app/src/main/java/com/example/appy/user.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+
+public class user {
+ String username = "";
+ String email = "";
+ String password = "";
+ ArrayList<String> followingList = new ArrayList<>();
+
+ public user(){}
+ public user(String usernameIn, String passwordIn){
+  username = usernameIn;
+  password = passwordIn;
+ }
+
+ public void setUsername(String name){
+  username = name ;
+ }
+
+ public void setEmail(String Email){
+  email = Email;
+ }
+
+ public void setPassword(String pass){
+  password = pass;
+ }
+ public void addFollower(String UName){
+  followingList.add(UName);
+ }
+
+ public void removerFollower (String userIn){
+  followingList.remove(userIn);
+  }
+
+
+ public String getUsername() {
+  return username;
+ }
+
+ public String getEmail() {
+  return email;
+ }
+
+ public ArrayList<String> getFollowingList() {
+  return followingList;
+ }
+
+ public boolean checkPassword(String InputPassword) {
+  if (password == InputPassword){return true;} else {return false;}
+ }
+}
